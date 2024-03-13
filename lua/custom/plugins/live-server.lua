46ddbf900d17546380c178ffc60d56e91ca885b1
Albydo <par.eggermont@gmail.com>
2024-03-13 12:00:50 +0100

    added codedium

--- lua/custom/plugins/live-server.lua
+++ lua/custom/plugins/live-server.lua
@@ -2,7 +2,5 @@ return {
   'barrett-ruth/live-server.nvim',
   build = 'pnpm add -g live-server',
   cmd = { 'LiveServerStart', 'LiveServerStop' },
-  config = function()
-    require('live-sever').setup {}
-  end,
+  config = true,
 }
